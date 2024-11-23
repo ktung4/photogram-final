738a60c908d41e32b7cf7be1342b690e2f0729d3
ktung4 <ktung@chicagobooth.edu>
2024-11-23 19:59:37 +0000

    In Process 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,31 @@ class UsersController < ApplicationController
     render(template: "users_html/index")
   end
 
+  def show
+    @username = params.fetch("username")
+    @the_user = User.where(username: @username).first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render(template: "users_html/show")
+    end
+  end
+
+  def create
+    my_input_username = params.fetch("input_username")
+    new_user = User.new
+    new_user.username = my_input_username
+    new_user.save
+    redirect_to("/users/" + my_input_username)
+  end
+
+  def update
+    user_id = params.fetch("user_id")
+    my_input_username = params.fetch("input_username")
+    the_user = User.where(id: user_id).first
+    the_user.username = my_input_username
+    the_user.save
+    redirect_to("/users/" + my_input_username)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get("/users/:username", controller: "users", action: "show")
   post("/add_user", controller: "users", action: "create")
   post("/update_user/:user_id", controller: "users", action: "update")
+
+
+
   get("/photos", controller: "photos", action: "index")
   get("/photos/:photo_id", controller: "photos", action: "show")
   get("/delete_photo/:photo_id", controller: "photos", action: "delete")
