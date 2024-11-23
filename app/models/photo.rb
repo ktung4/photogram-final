09fa5ea0f98a35e82cb6f3c543353222b6970a0d
ktung4 <ktung@chicagobooth.edu>
2024-11-23 17:10:20 +0000

    Created Tables

---
+++ app/models/photo.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id             :bigint           not null, primary key
+#  caption        :text
+#  comments_count :integer
+#  image          :string
+#  likes_count    :integer
+#  title          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  owner_id       :integer
+#
+class Photo < ApplicationRecord
+end
---
+++ db/migrate/20241123164832_create_photos.rb
@@ -0,0 +1,14 @@
+class CreatePhotos < ActiveRecord::Migration[7.1]
+  def change
+    create_table :photos do |t|
+      t.string :title
+      t.text :caption
+      t.integer :comments_count
+      t.string :image
+      t.integer :likes_count
+      t.integer :owner_id
+
+      t.timestamps
+    end
+  end
+end
